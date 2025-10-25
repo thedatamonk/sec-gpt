8e486fc5902d75545f0375762e03ce56a67d6a79
Rohil Pal <rohilpal9763@gmail.com>
2025-10-25 10:58:32 +0530

    Consolidate constants in a single file

--- mods/constants.py
+++ mods/constants.py
@@ -1,2 +1,4 @@
-RED = "\033[91m"
-RESET = "\033[0m"
+DEFAULT_OLLAMA_BASE_URL = "http://localhost:11434"
+DEFAULT_OLLAMA_MODEL = "llama3:instruct"
+DEFAULT_OPENAI_CHAT_MODEL = "gpt-3.5-turbo"
+SEC_COMPANY_TICKERS_URL = "https://www.sec.gov/files/company_tickers.json"
